8ef211aa86838c3e4177df9fbb37cc4316dffd46
arrogroup <arrogroup.laos@gmail.com>
2024-03-23 20:58:16 +0700

    0.1.5 test

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pylao',
+    version='0.1.5',
+    author='arrrogroup',
+    author_email='arrogroup.laos@email.com',
+    description='PyLao is Lao Libary',
+    long_description='PyLao is Lao Libary',
+    long_description_content_type='text/markdown',
+    url='https://github.com/arrogroup/pylao',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.9',
+)
